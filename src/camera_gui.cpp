343b3660d5c66a7721e2dbf8c4d79382c01d7ad9
malasiot <malasiot@gmail.com>
2024-03-23 10:56:19 +0200

    recorder

--- src/camera_gui.cpp
+++ src/camera_gui.cpp
@@ -7,6 +7,7 @@
 #include <QCommandLineParser>
 #include <QToolBar>
 #include <QStatusBar>
+#include <QToolButton>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -40,13 +41,21 @@ using namespace std ;
 
 class CameraGrabber: public rclcpp::Node {
 public:
-    CameraGrabber(const std::string &rgb_topic, const std::string &depth_topic, rclcpp::NodeOptions nh = rclcpp::NodeOptions()):
-        rclcpp::Node("camera_grabber_node", nh) {
+    CameraGrabber(const std::string &rgb_topic, const std::string &depth_topic,
+                  const std::string &cap_folder, const std::string &cap_prefix,
+                  rclcpp::NodeOptions nh = rclcpp::NodeOptions()):
+        cap_folder_(cap_folder), cap_prefix_(cap_prefix), rclcpp::Node("camera_grabber_node", nh) {
         subscribe(rgb_topic, depth_topic) ;
     }
 
     void setViewer(CameraViewer *viewer) {
         viewer_ = viewer ;
+    }
+
+    void setRecording(bool rec) {
+         std::lock_guard<std::mutex> lock(mutex_) ;
+         is_recording_ = rec ;
+         frame_number_ = 0 ;
     }
 
 private:
@@ -62,6 +71,15 @@ private:
     }
 
 
+
+    std::string formatName() const {
+        std::stringstream strm ;
+        strm << cap_prefix_ ;
+        strm << setw(5) << setfill('0') ;
+        strm << frame_number_ ;
+        return strm.str() ;
+    }
+
     void frameCallback(const sensor_msgs::msg::Image::ConstSharedPtr colorMsg, const sensor_msgs::msg::Image::ConstSharedPtr depthMsg) {
 
         try
@@ -69,7 +87,7 @@ private:
             auto colorPtr = cv_bridge::toCvCopy(colorMsg, sensor_msgs::image_encodings::BGR8);
 
             rgb_ = colorPtr->image;
-            frame_number_ ++ ;
+
             if ( viewer_->getChannel() == CameraViewer::CHANNEL_RGB ) {
                 QMetaObject::invokeMethod(viewer_, [=]() {
                     viewer_->setImage(rgb_);
@@ -83,6 +101,7 @@ private:
             // display the error at most once per 10 seconds
             RCLCPP_ERROR_THROTTLE(get_logger(), *this->get_clock(), 10, "cv_bridge exception %s at line number %d on function %s in file %s", e.what(), __LINE__,
                                   __FUNCTION__, __FILE__);
+            return ;
         }
 
         try
@@ -113,13 +132,38 @@ private:
             // display the error at most once per 10 seconds
             RCLCPP_ERROR_THROTTLE(get_logger(), *this->get_clock(), 10, "cv_bridge exception %s at line number %d on function %s in file %s", e.what(), __LINE__,
                                   __FUNCTION__, __FILE__);
+            return ;
         }
 
+
+        if ( is_recording_ ) {
+            std::lock_guard<std::mutex> lock(mutex_) ;
+
+            std::string file_name = formatName() ;
+
+            std::string rgb_path = cap_folder_ + '/' + file_name + "_c.png" ;
+            std::string depth_path = cap_folder_ + '/' + file_name + "_d.png" ;
+
+            cv::imwrite(rgb_path, rgb_) ;
+            cv::imwrite(depth_path, depth_) ;
+
+            frame_number_ ++ ;
+
+            QMetaObject::invokeMethod(viewer_, [=]() {
+                stringstream strm ;
+                strm << "Recorded " << frame_number_ << " frames" ;
+                viewer_->setStatus(strm.str());
+            }, Qt::ConnectionType::QueuedConnection);
+
+
+        }
 
 
     }
 
-    cv::Mat rgb_, depth_ ;
+     std::string cap_folder_, cap_prefix_ ;
+     cv::Mat rgb_, depth_ ;
+     bool is_recording_ = false ;
 
      rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr caminfo_sub_;
      message_filters::Subscriber<sensor_msgs::msg::Image> rgb_sub_ ;
@@ -130,11 +174,14 @@ private:
      std::unique_ptr<Synchronizer> sync_ ;
      size_t frame_number_ = 0 ;
      CameraViewer *viewer_ ;
+     std::mutex mutex_ ;
+
 };
 
 
 
-CameraViewer::CameraViewer(QWidget *parent):
+CameraViewer::CameraViewer(std::shared_ptr<CameraGrabber> grabber, QWidget *parent):
+    grabber_(grabber),
     QMainWindow(parent) {
 
     image_widget_ = new QImageWidget(this) ;
@@ -150,6 +197,13 @@ CameraViewer::CameraViewer(QWidget *parent):
     channel_combo_ = new QComboBox(this) ;
     channel_combo_->addItems(QStringList() << "RGB" << "Depth") ;
     tb->addWidget(channel_combo_) ;
+
+   record_action_ = new QAction(QIcon(":/images/capture.png"), "Record", this) ;
+   record_action_->setCheckable(true) ;
+   tb->addAction(record_action_) ;
+
+   connect(record_action_, &QAction::changed, this, &CameraViewer::onRecordingStateChanged) ;
+
     addToolBar(tb);
 
     statusBar()->showMessage("Ready");
@@ -165,6 +219,16 @@ CameraViewer::Channel CameraViewer::getChannel() const {
     else return CHANNEL_DEPTH ;
 }
 
+void CameraViewer::setStatus(const std::string &msg) {
+    statusBar()->showMessage(QString().fromStdString(msg)) ;
+}
+
+void CameraViewer::onRecordingStateChanged()
+{
+    grabber_->setRecording(record_action_->isChecked());
+
+}
+
 
 int main(int argc, char * argv[])
 {
@@ -175,6 +239,11 @@ int main(int argc, char * argv[])
     parser.addHelpOption();
     parser.addPositionalArgument("rgb_topic", QCoreApplication::translate("main", "Topic for RGB image."));
     parser.addPositionalArgument("depth_topic", QCoreApplication::translate("main", "Topic for Depth topic."));
+    QCommandLineOption folderOption("folder", "output folder", "out", "/tmp/") ;
+    QCommandLineOption prefixOption("prefix", "filename prefix", "prefix", "cap_") ;
+
+    parser.addOption(folderOption) ;
+    parser.addOption(prefixOption) ;
     parser.process(app);
 
     const QStringList args = parser.positionalArguments();
@@ -185,9 +254,11 @@ int main(int argc, char * argv[])
 
     rclcpp::init(argc, argv);
 
-    std::shared_ptr<CameraGrabber> grabber = std::make_shared<CameraGrabber>(args.at(0).toStdString(), args.at(1).toStdString()) ;
+    std::shared_ptr<CameraGrabber> grabber = std::make_shared<CameraGrabber>(args.at(0).toStdString(), args.at(1).toStdString(),
+                                                                             parser.value("folder").toStdString(),
+                                                                             parser.value("prefix").toStdString()) ;
 
-    CameraViewer window ;
+    CameraViewer window(grabber) ;
     window.resize(512, 512) ;
     window.show() ;
 
--- src/camera_gui.hpp
+++ src/camera_gui.hpp
@@ -6,11 +6,13 @@
 #include <QListView>
 #include <QStringListModel>
 #include <QComboBox>
+#include <QAction>
 
 #include <opencv2/opencv.hpp>
 
 #include "image_widget.hpp"
 
+class CameraGrabber ;
 
 class CameraViewer: public QMainWindow
 {
@@ -19,19 +21,24 @@ class CameraViewer: public QMainWindow
 public:
     enum Channel { CHANNEL_RGB, CHANNEL_DEPTH } ;
 
-    CameraViewer(QWidget *parent = 0);
+    CameraViewer(std::shared_ptr<CameraGrabber> grabber, QWidget *parent = 0);
 
     void setImage(const cv::Mat &im) ;
 
     Channel getChannel() const ;
 
+    void setStatus(const std::string &msg) ;
+
 private Q_SLOTS:
+    void onRecordingStateChanged() ;
 
 private:
     void enableButtons(bool enable) ;
 
     QComboBox *channel_combo_, *rgb_topics_, *depth_topics_, *camera_topics_ ;
     QImageWidget *image_widget_ ;
+    QAction *record_action_ ;
+    std::shared_ptr<CameraGrabber> grabber_ ;
 
  };
 
